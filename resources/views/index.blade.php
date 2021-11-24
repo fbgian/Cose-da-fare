784fb38922f1aa47f35ee7f6929694e893b79ca0
Fabio Giannetti <fgchaka@gmail.com>
2021-11-24 16:01:45 +0100

    PROVA cron

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -72,7 +72,7 @@
             <div class="card mb-4">
                 <div class="card-header">
                     <i class="fas fa-table me-1"></i>
-                    Lista da fare
+                    Lista da fare AGGIORNATA
                 </div>
                 <div class="card-body">
 
